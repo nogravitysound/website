7dc60184526185cfcde85a74b942b2e60b779db2
GioPan04 <gioele@pannetto.com>
2022-04-18 16:28:01 +0200

    Added inertia

---
+++ resources/js/Pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Head } from '@inertiajs/inertia-react';
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <Head title="Home" />
+            <span>Test</span>
+        </>
+    );
+};
+
+export default Home;
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>No Gravity Sound</title>
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+        @inertiaHead
     </head>
     <body class="antialiased">
-
+        @inertia
     </body>
 </html>
